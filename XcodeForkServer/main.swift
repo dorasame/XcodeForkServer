400e427c67ae98524d2379c78d4a2a494692335c
Limdongju <dongju721@naver.com>
2020-05-08 21:38:22 -0700

    Update in leader

--- XcodeForkServer/main.swift
+++ XcodeForkServer/main.swift
@@ -3,10 +3,11 @@
 //  XcodeForkServer
 //
 //  Created by 20190708 on 08/05/2020.
-//  Copyright © 2020 DongjuLim. All rights reserved.
+///Users/a20190708/Desktop/XcodeForkServer/XcodeForkServer.xcodeproj  Copyright © 2020 DongjuLim. All rights reserved.
 //
 
 import Foundation
 
 print("Hello World 1 in team leader server")
 
+print("Update in leader")
